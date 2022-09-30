62fd467a2f225314af350df310f9b211d365d9f1
meetagrawal09 <agrawalmeet91@gmail.com>
2022-09-30 18:28:25 +0530

    fixed by redirection

--- server/routes/home.js
+++ server/routes/home.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.redirect('http://localhost:3000');
 })
 
 module.exports = app;
